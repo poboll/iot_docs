59001f3c0c0c7db7216594e9c433bd9952fd220f
在虎 <caiths@icloud.com>
2023-08-12 23:34:50 +0800

    📝 更新文档

---
+++ theme.config.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Wps_Script</span>,
+  project: {
+    link: 'https://github.com/poboll/wps_script',
+  },
+  docsRepositoryBase: 'https://github.com/poboll/wps_docs',
+  footer: {
+    text: '© 2023 [温瞳](https://www.caiths.com)',
+  },
+}
+
+export default config
