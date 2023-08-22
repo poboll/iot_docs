2270cd2b94a354533ddb46dd6a0ea12e49e36ec2
在虎 <caiths@icloud.com>
2023-08-23 07:37:58 +0800

    📝 更新文档

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>Wps_Script</span>,
-  project: {
-    link: 'https://github.com/poboll/wps_script',
-  },
-  docsRepositoryBase: 'https://github.com/poboll/wps_docs',
+  logo: <span>Iot_docs</span>,
+  // project: {
+  //   link: 'https://github.com/poboll/wps_script',
+  // },
+  docsRepositoryBase: 'https://github.com/poboll/iot_docs',
   footer: {
     text: '© 2023 [温瞳](https://www.caiths.com)',
   },
