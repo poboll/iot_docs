bc0905c699985469513049e43b2e2cd69770fb07
在虎 <caiths@icloud.com>
2023-08-23 08:45:32 +0800

    Add files via upload

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,20 +2,11 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: (
-    <><img src="https://blog.itjoker.cn/favicon.ico" alt="Logo" />
-      <span>
-        Iot_docs
-      </span>
-    </>
-  ),
+  logo: <span >Iot_docs</span>,
   // project: {
   //   link: 'https://github.com/poboll/wps_script',
   // },
   docsRepositoryBase: 'https://github.com/poboll/iot_docs',
-  // footer: {
-  //   text: '© 2023 [温瞳](https://www.caiths.com)',
-  // },
 }
 
 export default {
