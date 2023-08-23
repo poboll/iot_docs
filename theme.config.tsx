03679fae4cfa61e29dd2d3d8787b848c7155cd13
在虎 <caiths@icloud.com>
2023-08-23 14:12:06 +0800

    Add files via upload

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,23 +1,17 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
-const config: DocsThemeConfig = {
-  logo: <span>Iot_docs</span>,
-  project: {
-    link: 'https://github.com/poboll/wps_script',
-  },
-  docsRepositoryBase: 'https://github.com/poboll/iot_docs',
-}
-
 export default {
-    // useNextSeoProps() {
-    //     const { asPath } = useRouter()
-    //     if (asPath !== '/') {
-    //         return {
-    //             titleTemplate: '%s – Iot_docs'
-    //         }
-    //     }
-    // },
+    logo: <span>Iot_docs</span>,
+    project: {
+        link: null,
+    },
+    docsRepositoryBase: null,
+    useNextSeoProps() {
+        return {
+            titleTemplate: '%s – Iot_docs'
+        }
+    },
     footer: {
         text: (
             <span>
