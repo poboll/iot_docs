1fa8fe06641a37605fb7f0930cef4990d2e08c49
在虎 <caiths@icloud.com>
2023-08-23 08:32:09 +0800

    Add files via upload

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,14 +3,8 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
   logo: (
-    <>
-      <svg width="24" height="24" viewBox="0 0 24 24">
-        <path
-            fill="currentColor"
-            d="m231.9 169.8l-94.8 65.6a15.7 15.7 0 0 1-18.2 0l-94.8-65.6a16.1 16.1 0 0 1-6.4-17.3L45 50a12 12 0 0 1 22.9-1.1L88.5 104h79l20.6-55.1A12 12 0 0 1 211 50l27.3 102.5a16.1 16.1 0 0 1-6.4 17.3Z"
-        />
-      </svg>
-      <span style={{ marginLeft: '.4em', fontWeight: 800 }}>
+    <><img src="https://blog.itjoker.cn/favicon.ico" alt="Logo" />
+      <span>
         Iot_docs
       </span>
     </>
@@ -36,7 +30,7 @@ export default {
     footer: {
         text: (
             <span>
-            MIT {new Date().getFullYear()} ©{' 2023 '}
+            MIT {new Date().getFullYear()} ©{' '}
                 <a href="https://www.caiths.com" target="_blank">
             温瞳
     </a>
