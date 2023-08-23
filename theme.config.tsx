7391240b23c0b35ca3d72c743bec6715c26d1e98
在虎 <caiths@icloud.com>
2023-08-23 13:56:18 +0800

    Add files via upload

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span >Iot_docs</span>,
-  // project: {
-  //   link: 'https://github.com/poboll/wps_script',
-  // },
+  logo: <span>Iot_docs</span>,
+  project: {
+    link: 'https://github.com/poboll/wps_script',
+  },
   docsRepositoryBase: 'https://github.com/poboll/iot_docs',
 }
 
